f1ef07c5a33a5f64b7d8f91a77524a3b629e6d52
Vitor Martins <vitor.martins@fever.pt>
2021-07-01 23:49:30 +0100

    ADD roomCode and newRoom -  wip 3rd day

--- LetMeAsk.Static/src/pages/Room.tsx
+++ LetMeAsk.Static/src/pages/Room.tsx
@@ -1,1 +1,46 @@
-export {}
+
+import { useParams } from 'react-router'
+import { Button } from '../components/Button'
+import { RoomCode } from '../components/RoomCode'
+
+import logoImg from '../assets/images/logo.svg'
+
+import '../styles/room.scss'
+
+type RoomParams = {
+	id: string;
+}
+
+
+export function Room() {
+	const params = useParams<RoomParams>()
+
+
+	return ( 
+		<div id="page-room">
+			<header>
+				<div className='content'>
+					<img src={logoImg} alt="Letmeask" />
+					<RoomCode code={params.id}/>
+				</div>
+			</header>
+
+			<main>
+				<div className='room-title'>
+					<h1>Sala React</h1>
+					<span>4 perguntas</span>
+				</div>
+				<form>
+					<textarea 
+						placeholder='O que você quer perguntar?' 
+					/>
+					<div className='form-footer'>
+						<span><button>faça seu login</button></span>
+						<Button type='submit'>Enviar perguntas</Button>
+					</div>
+				</form>
+			</main>
+
+		</div>
+	)
+}
